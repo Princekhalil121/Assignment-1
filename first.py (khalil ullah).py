fc6e087bb26a7c281341e72f36af3b3f0faa4666
Princekhalil121 <p.khalilullah786@gmail.com>
2024-10-30 19:49:10 +0500

    Assignment no1
    We Are created our 1 Assignment in python with vs code

---
+++ first.py (khalil ullah).py
@@ -0,0 +1,38 @@
+number1=8
+number2=17
+number3=16
+
+# calculate sum
+sum=number1+number2+number3
+print("sum:",sum)
+
+# calculate difference
+diffference=number1-number2-number3
+print("difference:",diffference)
+
+# calculate product
+product=number1*number2*number3
+print("product:",product)
+
+# calculate average
+average=(number1+number2+number3)
+print("average:",average)
+#area of triangle
+breathe=7
+height=5
+area=0.5*breathe*height
+print("triangle area",area)
+#area of square
+side=4
+area=side*side
+print("square area",area)
+#area of circle
+p1=4.14
+radius=4
+area=p1*radius*radius
+print("circle area",area)
+#rectangle area
+length=12
+width=8
+area=length*width
+print("rectangle area",area)
